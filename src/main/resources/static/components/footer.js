8813510cdbf0fc8399591228395eadba6906c26b
jhector1 <jeanyveshector@gmail.com>
2022-11-13 01:36:14 -0600

    multiple commit

--- src/main/resources/static/components/footer.js
+++ src/main/resources/static/components/footer.js
@@ -10,52 +10,10 @@ class Footer extends HTMLElement {
 
       <style>
 
-        footer {
-    position: relative;
-    background-color: #46464a;
-    border: blue 2px solid;
-    height: 400px;
-    margin-top: 50px;
-}
-
-footer .link, footer .contact, footer .logo {
-    padding: 50px;
-    display: inline-block;
-}
-
- .logo2 {
-    position: absolute;
-    display: inline-block;
-    width: 30%;
-    left: 50px;
-
-}
-
-footer .link {
-    position: absolute;
-    text-decoration: none;
-    left: 550px;
-
-}
-
-.contact {
-    background-color: #46464a;
-    position: absolute;
-    right: 50px;
-
-
-}
-
-.contact li {
-
-    text-align: center;
-    padding: 14px 30px;
-    text-decoration: none;
-    font-size: 20px;
-}
+      
       </style><title></title>
       </head>
-     <footer>
+     <footer id="myFooter">
     <div class="logo2">
         <img src="/images/bainLogo.PNG"/>
     </div>
@@ -74,7 +32,7 @@ footer .link {
             <li>+1 (561) 856-5540</li>
             <li>baincommunication@gmail.com</li>
             <li>FOLLOW US</li>
-            <li>
+            <li class="social-media">
                 <a href="#" class="fa fa-linkedin"></a>
                 <a href="#" class="fa fa-youtube"></a>
                 <a href="https://www.facebook.com/profile.php?id=726930617" class="fa fa-facebook" target="_blank" ></a>
@@ -87,4 +45,10 @@ footer .link {
     }
 }
 
+
 customElements.define('footer-component', Footer);
+if (page !== "") {
+    changeColor("li a", "#46464a");
+    changeBackgroundColor("#myFooter","#6A6A75" )
+
+}
--- src/main/resources/static/components/header.js
+++ src/main/resources/static/components/header.js
@@ -11,51 +11,122 @@ class Header extends HTMLElement {
 
 
  header {
-
+justify-content: space-around;
+align-items: center;
     display: flex;
-    position: relative;
     background-color: #46464a;
     height: 60px;
     z-index: 1;
 
 
 }
+#bar{
+font-size: 2em;
+ margin: 5px;
+ display: none;
+}
 
-
-header .nav-link {
+ .nav-link{
     list-style-type: none;
-
-    position: absolute;
+    display: flex;
+    align-items: center;
+width: 650px;
+height: auto;
     right: 50px;
-    display: inline-block ;
     border: blue solid 4px;
 }
 
 
-.nav-link li {
-    float: left;
-    /*border-right: 1px solid #bbb;*/
-    display: block;
+.logo{
+width: 60px;
+height: auto;
+} .triangle{
+display: none;
+}
+
+@media screen and (max-width: 800px){
+.triangle{
+position: absolute;
+right: 0;
+display: inline-block;
+margin-right: 20px;
+bottom: 45%;
+} .triangle {
+font-size: 4em;
+color: #6A6A75;
+}
+header {
+display: block;
+
+position: fixed;
+left: -13em;
+top:0;
+padding-right:150px;
+width: 20em;
+border-right: solid #F8F8F8 4px;
+min-width: 300px;
+  transition: left 1s linear 3.4ms;
+
+height: 100%;
 
 
 }
+header:hover{
+padding: 0;
+left: 0;
+}
+article, footer, .photo_Background:not(header){
+    margin-left: 6.280em;
+}
+
+header .logo{ margin: auto;
+width: 80%;
+height: auto;
+
+}
+#bar{
+display: block;
+position: fixed;
+top: 2px;
+}
+
+#nav1 {
+ width: 50%;
+ height: 50%;
+ display: grid;
+ margin-left: 10%;
+ margin-top: 5%;
+}
+ .nav-link li {
+ width: 100%;
+display: block;
+padding: 
+float: none;
+}
+
+}
+
 a:active {
     color: yellow;
     background-color: transparent;
 }
  .logo img{
-    width:5%;
-    position: absolute;
-    left: 50px;
-    bottom:0;
+   width: 100%; 
+
+  
 }
-.nav-link li {
-    float: left;
-    /*border-right: 1px solid #bbb;*/
-    display: block;
+.nav-link{
+width: 650px;
+height: 60px;
+list-style-type: none;
+}
+/*.nav-link li {*/
+
+/*    float: left;*/
+/*    */
 
 
-}
+/*}*/
 
 li {
     border-right: none;
@@ -67,9 +138,11 @@ li a {
     padding: 14px 30px;
     text-decoration: none;
     font-size: 20px;
-    display: block;
+    display: inline;
     font-weight: bold;
 }
+
+
 
 a:hover {
     color: #AEADB2;
@@ -79,25 +152,36 @@ a:active {
     color: yellow;
     background-color: transparent;
 }
+
 .sticky {
     position: fixed;
     top: 0;
     width: 100%;
 }
 .photo_Background {
+display: flex;
+flex-wrap: wrap;
     height: 300px;
     background-image: url(../images/TVPhoto/tvphoto6.jpg);
     background-position: center;
+    justify-content: center;
 }
  .photo_Background h1 {
     padding-top: 100px;
     color: #FBFBFB;
-    font-size: 80px;
+    font-size: 4rem;
     font-weight: bolder;
     text-shadow: 5px 5px 10px black;
      text-align: center;
     background-position: center;
 }
+
+
+
+ /*.photo_Background h1{*/
+ /*   font-size: 60px;*/
+ /*}*/
+
 /*dropdown styling section*/
 .dropbtn {
   background-color: transparent;
@@ -106,13 +190,11 @@ a:active {
   font-size: 20px;
   font-weight: bold;
   border: none;
+  display: inline;
   cursor: pointer;
 }
 
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
+
 
 .dropdown-content {
   display: none;
@@ -134,12 +216,11 @@ a:active {
 
 .dropdown-content a:hover {color: #AEADB2;
     background-color: transparent;}
- .dropbtn i:hover{
- transform: rotate:( 60deg);
- }
+ 
 
-.dropdown:hover .dropdown-content {
-  display: block;
+.dropdown:hover .dropdown-content, .dropdown-content li {
+  display: grid;
+  
 }
 
 .dropdown:hover .dropbtn {
@@ -153,23 +234,26 @@ a:active {
     <h1>Bain Communications</h1>
  </div>
 <header id="myHeader">
+<div class="triangle"><i class="fa fa-play"></i></div>
     <div class="logo">
         <img src="../images/bainLogo91.png" alt="logo"/>
-    </div>
-    <ul class="nav-link">
+   </div>
+    <ul class="nav-link" id="nav1">
         <li class="li-nav"><a href="/">Home</a></li>
         <li class="li-nav"><a href="/about">About</a></li>
-        <li class="li-nav"><div class="dropdown">
-  <button class="dropbtn">Services<i style='padding-left:10px' class='fa fa-chevron-down'></i></button>
-  <div class="dropdown-content">
-  <a href="#">Phone Consultation</a>
-  <a href="/reserve">Home Consultation</a>
-  </div>
-</div></li>
+        <li class="dropdown">
+            <span  style="z-index: 3; " class="dropbtn"> Services<i style='padding-left:10px' class='fa fa-chevron-down'></i></span>
+            <ul class="dropdown-content">
+                 <li class="li-nav"><a href="#">Phone Consultation</a></li>
+                 <li class="li-nav"><a href="/reserve">Home Consultation</a></li>
+            </ul>
+        </li>
         <li class="li-nav"><a href="#">Reviews</a></li>
         <li class="li-nav"><a href="/contact">Contact</a></li>
     </ul>
+    
 </header>
+
  </body>
     `;
     }
@@ -187,23 +271,48 @@ function myFunction() {
     }
 }
 
-
-
 var path = window.location.pathname;
 var page = path.split("/").pop();
-if(page!==""){
-    let head_nav =document.querySelector("#myHeader");
-    document.querySelector(".photo_Background").style.display="none";
-    head_nav.style.top="0";
-    head_nav.style.position="sticky";
-    head_nav.style.backgroundColor="#F8F8F8";
-    head_nav.style.borderBottom="grey 1px solid";
-    document.querySelector(".nav-link").style.border="none";
+
+if (page !== "") {
+    let head_nav = document.querySelector("#myHeader");
+    let dropdown_content = document.querySelector(".dropdown-content");
+    head_nav.style.borderBottom = "grey 1px solid";
+
+changeBackgroundColor("#myHeader", "#6A6A75");
+    changeBackgroundColor(".dropdown-content", "#303035");
+    changeColor("li a", "#46464a");
+    changeColor(".dropbtn", "#46464a");
+    changeColor(".dropbtn i", "#46464a");
+    changeColor(".triangle", "#46464a");
 
 
+
+    dropdown_content.style.border = "grey 1px solid";
+    dropdown_content.style.borderTop = "none";
+    document.querySelector("#myFooter").style.backgroundColor = "#6A6A75";
+
+    document.querySelector(".nav-link").style.border = "none";
+
+    document.querySelector(".photo_Background").style.display = "none";
+
+} else {
+    if (window.onresize > 600) {
+        window.onscroll = function () {
+            myFunction()
+        };
+    }
 }
-else{
-    window.onscroll = function () {
-        myFunction()
-    };
+
+function changeBackgroundColor(tag, color){
+   const nodeList= document.querySelectorAll(tag);
+    for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].style.backgroundColor = color;
+    }
+}
+function changeColor(tag, color){
+    const nodeList= document.querySelectorAll(tag);
+    for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].style.color = color;
+    }
 }
